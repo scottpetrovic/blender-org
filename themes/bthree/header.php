547a74d7e6267efedef3958fee9d59a6da99e2b5
venomgfx <venomgfx@fraka.(none)>
2012-11-26 22:06:38 +0100

    blender logo on header!

--- themes/bthree/header.php
+++ themes/bthree/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Hello &middot; blender.org</title>
+		<title>blender.org - Home</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
 		<meta name="author" content="">
@@ -40,7 +40,7 @@
 	            <span class="icon-bar"></span>
 	            <span class="icon-bar"></span>
 	          </a>
-	          <a class="brand" href="/">blender.org</a>
+	          <a class="brand" href="/" ></a>
 	          <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
 	          <div class="nav-collapse collapse">
 	            <ul class="nav">

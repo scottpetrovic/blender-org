cf2476169507032df22832883a5bc24fed9d07d2
fsiddi <francesco.siddi@gmail.com>
2012-11-26 23:39:29 +0100

    Added basic widget infrastructure
    Added sidebar template
    Registered sidebar in functions

--- themes/bthree/page-with-sidebar.php
+++ themes/bthree/page-with-sidebar.php
@@ -7,6 +7,8 @@ Template Name: Page with Sidebar
 <?php get_header(); ?>
 
 		<div class="container">
+			<div class="row">
+				<div class="span8">
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
@@ -23,7 +25,10 @@ Template Name: Page with Sidebar
 
 
 				<?php endwhile; // end of the loop. ?>
-
+				</div>
+				<div class="span4">
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
 		</div>
-
 <?php get_footer(); ?>
---
+++ themes/bthree/sidebar.php
@@ -0,0 +1,5 @@
+<ul id="sidebar">
+	<?php if ( ! dynamic_sidebar() ) :
+		dynamic_sidebar( $sidebar );
+    endif; ?>
+</ul>

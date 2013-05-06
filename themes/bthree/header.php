c212c5dd600e9d4e0de41275c7e751d007eb1bfc
fsiddi <francesco.siddi@gmail.com>
2013-05-06 21:50:40 +0200

    Trying to catch up with the crap

--- themes/bthree/header.php
+++ themes/bthree/header.php
@@ -18,7 +18,7 @@
 		<?php
 		$template_name = get_page_template_name();
 		if ($template_name == 'page-download') { ?>
-			<link href="<?php bloginfo( 'template_directory' ); ?>/assets/css/page_download.css" rel="stylesheet">
+		<link href="<?php bloginfo( 'template_directory' ); ?>/assets/css/page_download.css" rel="stylesheet">
 		<? } ?>
 		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 		<!--[if lt IE 9]>
@@ -35,81 +35,8 @@
 
 	<body>
 
-<<<<<<< HEAD
-
-		<div class="header">
-			<div class="container">
-=======
-<?php 
-	function page_ancestry() { 
-		//generating an array with page ids 
-		//with the top most page in array element $page_tree[0]
-		global $post;
-		$this_page = $post;
-		$test = true;
-		$page_lineage = array();
-		$page_lineage[] = $this_page->ID; 
-		  while( $test ) :
-		    if($this_page->post_parent) :
-		    $page_lineage[] = $this_page->post_parent;
-		    $this_page = get_page($this_page->post_parent);
-		    else :
-		    $test = false;
-		    endif;
-		  endwhile;
-		$page_tree = array_reverse($page_lineage);
-		return $page_tree;
-	}
-	
-	/*
-	function top_level_list_pages() {
-		?>
-		<div class="layer-parent">
-			<?php wp_page_menu('depth=1'); ?>
-		</div> <!--/ .layer-parent -->
-		<?php } 
-	*/
-	
-	function child_level_list_pages() {
-		global $post; 
-		$pages_tree = page_ancestry(); 
-		if($pages_tree[0]) : 
-			$this_page = $pages_tree[0]; 
-			// find a condition that shows this page and its brothers/sisters in the ancestry of the current page
-			$plist = wp_list_pages('title_li=&child_of=' . $this_page .'&depth=1&echo=0'); 
-			if(plist) { ?>
-				<div class="navbar second_level">
-					<ul class="nav">
-						<?php echo $plist; ?>
-					</ul>
-					<div class="clearfix"></div>
-				</div> <!--/ .second-level -->
-			<?php }
-		endif; //end if($pages_tree[0]) 
-	}
-	
-	function grandchild_level_list_pages() { 
-		global $post; 
-		$pages_tree = page_ancestry(); 
-		if($pages_tree[1]) : 
-			$this_page = $pages_tree[1]; 
-			// find a condition that shows this page and its brothers/sisters in the ancestry of the current page
-			$plist = wp_list_pages('title_li=&child_of=' . $this_page .'&depth=1&echo=0'); 
-			if(plist) { ?>
-				<div class="navbar third_level">
-					<ul class="nav">
-						<?php echo $plist; ?>
-					</ul>
-					<div class="clearfix"></div>
-				</div> <!--/ .third-level -->
-			<?php }
-		endif; //end if($pages_tree[1]) 
-	}
-	
-?>
 		<div class="container">
 			<div class="header">
->>>>>>> 8367549cadc3c36d7712b9fe88dcfe94a6742fc7
 			    <div class="navbar">
 					<a class="logo" href="<?php echo site_url(); ?>"></a>
 				    <div class="navbar-inner">
@@ -131,15 +58,8 @@
 			          </a>
 				    <div class="clearfix"></div>
 			    </div> <!--  // NAV BAR -->
-<<<<<<< HEAD
-			    <?php second_level_nav();?>
-			    <div class="clearfix"></div>
-			    <?php third_level_nav();?>
-			</div>
-=======
-				    <?php child_level_list_pages();?>
-				    <?php grandchild_level_list_pages();?>
->>>>>>> 8367549cadc3c36d7712b9fe88dcfe94a6742fc7
+				    <?php second_level_nav();?>
+				    <?php third_level_nav();?>
 
 			</div>
 			 <div class="clearfix"></div>

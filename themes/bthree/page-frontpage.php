40e4098006ca8b0bfd5a4ed37ea9803f0696ab3a
venomgfx <venomgfx@fraka.(none)>
2013-05-31 01:33:41 +0200

    Remove hardcoded content, load from the actual post instead

--- themes/bthree/page-frontpage.php
+++ themes/bthree/page-frontpage.php
@@ -7,26 +7,21 @@ Template Name: Frontpage
 <div class="frontpage">
 <?php get_header('carousel'); ?>
 </div>
-<div class="container sections">
-  <!-- Three columns of text below the carousel -->
-  <div class="row">
-    <div class="span4">
-      <h2>Support</h2>
-      <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-      <p><a class="btn" href="#">View details &raquo;</a></p>
-    </div><!-- /.span4 -->
-    <div class="span4">
-      <h2>Get Involved</h2>
-      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-      <p><a class="btn" href="#">View details &raquo;</a></p>
-    </div><!-- /.span4 -->
-    <div class="span4">
-      <h2>News</h2>
-      <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-      <p><a class="btn" href="#">View details &raquo;</a></p>
-    </div><!-- /.span4 -->
-  </div><!-- /.row -->
-
+	<div class="container sections">
+		<div class="row">
+			<div class="span12">
+			<?php while ( have_posts() ) : the_post(); ?>
+	
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-<?php the_ID(); ?> -->
+	
+			<?php endwhile; // end of the loop. ?>
+			</div>
+		</div>
+	</div>
   <div id="push"></div>
 </div>
 <?php get_footer('sitemap'); ?>
